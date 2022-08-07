c2828626dbcaca7ab11358d95313f88d4a7f811a
Tanner-Gladson <tannergladson1@gmail.com>
2022-08-07 18:11:03 -0400

    Created stack strcture

--- stack.py
+++ stack.py
@@ -23,10 +23,33 @@ class Stack(LinkedList):
         LinkedList.__init__(self)
     
     def push(self, new_node: LinkedNode) -> None:
+        '''
+        Add an element to the top of the stack
+        '''
         self.prepend(new_node)
         
     def pop(self) -> LinkedNode:
+        '''
+        Remove & return the top element of the stack
+        '''
         return self.remove_after(None)
+    
+    def peek(self):
+        '''
+        Return the top node in the stack
+        '''
+        if self.head == None:
+            return None
+        else:
+            return self.head
+            
+    def isEmpty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+    
+    
         
 if __name__ == "__main__":
     myStack = Stack()
@@ -36,6 +59,6 @@ if __name__ == "__main__":
     nodeC = LinkedNode(3)
     
     myStack.push(nodeA)
-    myStack.pop()
     
     print(myStack)
+    print(myStack.peek().value)
